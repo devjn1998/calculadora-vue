18852ae9362da5a6b318b2e32e04ce93fe11e183
Juan Costa <juandev1998@gmail.com>
2024-05-16 18:45:39 -0300

    quinto commit

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -16,17 +16,20 @@ const operator = ref(null)
   <div class="row">
     <div class="col-4 text-center">
       <label class="">NUMERO 1</label>
-      <input type="number" v-model="number_1" class="form-control" />
+      <input type="number" v-model="number_1" class="form-control" @change="emit('calculate', { n1: number_1, n2: number_2, operator: operator })" />
     </div>
     <div class="col-4 text-center">
       <label class="">NUMERO 2</label>
-      <input type="number" v-model="number_2" class="form-control" />
+      <input type="number" v-model="number_2" class="form-control" @change="emit('calculate', { n1: number_1, n2: number_2, operator: operator })" />
     </div>
 
     <div class="col-4 text-center">
       <label>OPERADOR</label>
-      <select class="form-control" v-model="operator"
-        @change="emit('calculate', { n1: number_1, n2: number_2, operator: operator })">
+      <select
+        class="form-control"
+        v-model="operator"
+        @change="emit('calculate', { n1: number_1, n2: number_2, operator: operator })"
+      >
         <option class="text-center" value="+">+ ADIÇÃO</option>
         <option class="text-center" value="-">- SUBTRAÇÃO</option>
         <option class="text-center" value=""> MULTIPLICAÇÃO</option>
